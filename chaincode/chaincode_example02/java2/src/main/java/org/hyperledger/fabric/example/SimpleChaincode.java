4a3963dcbee9517b78f090e82675e3aa73c507c5
stone-ch <great_cy_ang@163.com>
2019-05-15 14:49:29 +0800

    add bin str to str

--- chaincode/chaincode_example02/java2/src/main/java/org/hyperledger/fabric/example/SimpleChaincode.java
+++ chaincode/chaincode_example02/java2/src/main/java/org/hyperledger/fabric/example/SimpleChaincode.java
@@ -1,6 +1,7 @@
 package org.hyperledger.fabric.example;
 
 import java.util.List;
+import java.util.Iterator;
 
 import com.google.protobuf.ByteString;
 import io.netty.handler.ssl.OpenSsl;
@@ -8,6 +9,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hyperledger.fabric.shim.ChaincodeBase;
 import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
+import org.hyperledger.fabric.shim.ledger.KeyModification;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -17,10 +21,6 @@ public class SimpleChaincode extends ChaincodeBase {
 
     @Override
     public Response init(ChaincodeStub stub) {
-        List<String> args = stub.getStringArgs();
-        if (args.size() != 3) {
-            return newErrorResponse("Incorrect number of arguments. Expecting 3");
-        }
         return newSuccessResponse();
     }
 
@@ -33,91 +33,205 @@ public class SimpleChaincode extends ChaincodeBase {
             if (func.equals("Put")) {
                 return Put(stub, params);
             }
+            if (func.equals("PutBin")) {
+                return PutBin(stub, params);
+            }
+            if (func.equals("GetBin")) {
+                return GetBin(stub, params);
+            }
             if (func.equals("Get")) {
                 return Get(stub, params);
             }
-            if (func.equals("getHistoryByKey")) {
-                return getHistoryByKey(stub, params);
+            if (func.equals("GetHistoryByKey")) {
+                return GetHistoryByKey(stub, params);
             }
-            if (func.equals("getRecordByKey")) {
-                return getRecordByKey(stub, params);
+            if (func.equals("GetRecordByKey")) {
+                return GetRecordByKey(stub, params);
             }
-            return newErrorResponse("Invalid invoke function name. Expecting one of: [\"Put\", \"Get\", \"getHistoryByKey\", \"getRecordByKey\"]");
+            return newErrorResponse("Invalid invoke function name. Expecting one of: [\"Put\", \"Get\", \"getHistoryByKey\"]");
         } catch (Throwable e) {
             return newErrorResponse(e);
         }
     }
 
+    // TODO verify record signature
+    private boolean verify(String jsonStr, String signatureStr, String pubkeyPemStr){
+        // String hash = hashCal(jsonStr);
+        return true;
+    }
+
+    // TODO calculate string hash value
+    private String hashCal(String str){
+        return "Hash String";
+    }
+
+    // API:Put records
     private Response Put(ChaincodeStub stub, List<String> args){
-        return newSuccessResponse(ByteString.copyFrom("put function", UTF_8).toByteArray());
-        // if (args.size() != 3) {
-        //     return newErrorResponse("Incorrect number of arguments. Expecting 3");
-        // }
-        // String accountFromKey = args.get(0);
-        // String accountToKey = args.get(1);
+        int argsNeeded = 4;
+        if (args.size() != 4){
+            return newErrorResponse("Incorrect number of arguments. Expecting " + argsNeeded);
+        }
+        String putKey = args.get(0);
+        String jsonStr = args.get(1);
+        String signatureStr = args.get(2);
+        String pubkeyPemStr = args.get(3);
 
-        // String accountFromValueStr = stub.getStringState(accountFromKey);
-        // if (accountFromValueStr == null) {
-        //     return newErrorResponse(String.format("Entity %s not found", accountFromKey));
-        // }
-        // int accountFromValue = Integer.parseInt(accountFromValueStr);
+        if (!verify(jsonStr, signatureStr, pubkeyPemStr)){
+            return newErrorResponse("Incorrect number of arguments. Expecting " + argsNeeded);
+        }
 
-        // String accountToValueStr = stub.getStringState(accountToKey);
-        // if (accountToValueStr == null) {
-        //     return newErrorResponse(String.format("Entity %s not found", accountToKey));
-        // }
-        // int accountToValue = Integer.parseInt(accountToValueStr);
+        stub.putStringState(putKey, jsonStr);
+        _logger.info("Transfer complete");
+        return newSuccessResponse("invoke finished successfully");
+    }
+    
+    // Put binary string data on chain
+    private Response PutBin(ChaincodeStub stub, List<String> args){
+        int argsNeeded = 4;
+        if (args.size() != 4){
+            return newErrorResponse("Incorrect number of arguments. Expecting " + argsNeeded);
+        }
+        String putKey = args.get(0);
+        String jsonStr = args.get(1);
+        String signatureStr = args.get(2);
+        String pubkeyPemStr = args.get(3);
 
-        // int amount = Integer.parseInt(args.get(2));
+        String binary = "1100001 1100010 1100011";
+        jsonStr = BinToString(binary);
 
-        // if (amount > accountFromValue) {
-        //     return newErrorResponse(String.format("not enough money in account %s", accountFromKey));
-        // }
+        if (!verify(jsonStr, signatureStr, pubkeyPemStr)){
+            return newErrorResponse("Incorrect number of arguments. Expecting " + argsNeeded);
+        }
 
-        // accountFromValue -= amount;
-        // accountToValue += amount;
-
-        // _logger.info(String.format("new value of A: %s", accountFromValue));
-        // _logger.info(String.format("new value of B: %s", accountToValue));
-
-        // stub.putStringState(accountFromKey, Integer.toString(accountFromValue));
-        // stub.putStringState(accountToKey, Integer.toString(accountToValue));
-
-        // _logger.info("Transfer complete");
-
-        // return newSuccessResponse("invoke finished successfully", ByteString.copyFrom(accountFromKey + ": " + accountFromValue + " " + accountToKey + ": " + accountToValue, UTF_8).toByteArray());
+        stub.putStringState(putKey, jsonStr);
+        _logger.info("Transfer complete");
+        return newSuccessResponse("invoke finished successfully");
     }
 
-    // query callback representing the query of a chaincode
+    // Query callback representing the query of a chaincode
     private Response Get(ChaincodeStub stub, List<String> args) {
-        return newSuccessResponse("invoke Get function", ByteString.copyFrom("get function", UTF_8).toByteArray());
-        // if (args.size() != 1) {
-        //     return newErrorResponse("Incorrect number of arguments. Expecting name of the person to query");
-        // }
-        // String key = args.get(0);
-        // //byte[] stateBytes
-        // String val	= stub.getStringState(key);
-        // if (val == null) {
-        //     return newErrorResponse(String.format("Error: state for %s is null", key));
-        // }
-        // _logger.info(String.format("Query Response:\nName: %s, Amount: %s\n", key, val));
-        // return newSuccessResponse(val, ByteString.copyFrom(val, UTF_8).toByteArray());
+        if (args.size() != 1) {
+            return newErrorResponse("Incorrect number of arguments. Expecting name of the person to query");
+        }
+        String key = args.get(0);
+        StringBuilder strBuilder = new StringBuilder("");
+        strBuilder.append("[");
+        boolean shouldAddComma = false;
+        QueryResultsIterator<KeyValue> resultsIterator = stub.getQueryResult("{\"selector\":" + key + "}");
+        Iterator<KeyValue> iter = resultsIterator.iterator();
+
+        while(iter.hasNext())
+        {
+            if(shouldAddComma){
+                strBuilder.append(",");
+            }
+            KeyValue kval = iter.next();
+            strBuilder.append("{\"Key\":\"" + kval.getKey() + "\",\"Record\":" + kval.getStringValue() + "}");
+            _logger.info(String.format("result Key: %s, value: %s", kval.getKey(), kval.getStringValue()));
+            shouldAddComma = true;
+        }
+        strBuilder.append("]");
+
+        _logger.info("total result: " + strBuilder.toString());
+        return newSuccessResponse(ByteString.copyFrom(strBuilder.toString(), UTF_8).toByteArray());
     }
 
-    private Response getHistoryByKey(ChaincodeStub stub, List<String> args) {
-        return newSuccessResponse("invoke getHistoryByKey function");
-        // if (args.size() != 1) {
-        //     return newErrorResponse("Incorrect number of arguments. Expecting 1");
-        // }
-        // String key = args.get(0);
-        // // Delete the key from the state in ledger
-        // stub.delState(key);
-        // return newSuccessResponse();
+    // Query record by key, and change the binary string to string
+    private Response GetBin(ChaincodeStub stub, List<String> args) {
+        if (args.size() != 1) {
+            return newErrorResponse("Incorrect number of arguments. Expecting name of the person to query");
+        }
+        String key = args.get(0);
+        String val = stub.getStringState(key);
+        String binVal = StrToBinstr(val);
+        if (val == null) {
+            return newErrorResponse(String.format("Error: state for %s is null", key));
+        }
+        _logger.info(String.format("Query Response:\nName: %s, Amount: %s\n", key, binVal));
+        return newSuccessResponse(binVal, ByteString.copyFrom(binVal, UTF_8).toByteArray());
     }
 
-    private Response getRecordByKey(ChaincodeStub stub, List<String> args) {
-        return newSuccessResponse("invoke getRecordByKey function");
+    // API:Get the history of a key
+    private Response GetHistoryByKey(ChaincodeStub stub, List<String> args) {
+        if (args.size() != 1) {
+            return newErrorResponse("Incorrect number of arguments. Expecting 1");
+        }
+        String key = args.get(0);
+        StringBuilder strBuilder = new StringBuilder("");
+        strBuilder.append("[");
+        boolean shouldAddComma = false;
+        QueryResultsIterator<KeyModification> resultsIterator = stub.getHistoryForKey(key);
+        Iterator<KeyModification> iter = resultsIterator.iterator();
+ 
+        while(iter.hasNext())
+        {
+            if(shouldAddComma){
+                strBuilder.append(",");
+            }
+            KeyModification kval = iter.next();
+            strBuilder.append("{\"TxId\":\"" + kval.getTxId() + "\",\"Record\":" + kval.getStringValue() + ",\"Timestamp\":\""+ kval.getTimestamp() + "\",\"IsDeleted\":\"" + kval.isDeleted() + "\"}");
+            _logger.info(String.format("result TxId: %s, value: %s", kval.getTxId(), kval.getStringValue()));
+            shouldAddComma = true;
+        }
+        strBuilder.append("]");
+
+        _logger.info("total result: " + strBuilder.toString());
+        return newSuccessResponse(ByteString.copyFrom(strBuilder.toString(), UTF_8).toByteArray());
     }
+
+    // API:Get record by key
+    private Response GetRecordByKey(ChaincodeStub stub, List<String> args) {
+        if (args.size() != 1) {
+            return newErrorResponse("Incorrect number of arguments. Expecting name of the person to query");
+        }
+        String key = args.get(0);
+        String val = stub.getStringState(key);
+        if (val == null) {
+            return newErrorResponse(String.format("Error: state for %s is null", key));
+        }
+        _logger.info(String.format("Query Response:\nName: %s, Amount: %s\n", key, val));
+        return newSuccessResponse(val, ByteString.copyFrom(val, UTF_8).toByteArray());
+    }
+
+    // Binary String to string
+    private static String BinToString(String binary) { 
+        String[] tempStr = binary.split(" "); 
+        char[] tempChar = new char[tempStr.length]; 
+        for(int i = 0; i < tempStr.length; i++) { 
+            tempChar[i] = BinstrToChar(tempStr[i]); 
+        } 
+        return String.valueOf(tempChar); 
+    }
+
+    // Binary String to int array
+    private static int[] BinstrToIntArray(String binStr) { 
+        char[] temp = binStr.toCharArray(); 
+        int[] result = new int[temp.length];  
+        for(int i = 0; i < temp.length; i++) {  
+            result[i] = temp[i] - 48;       
+        } 
+        return result; 
+    }
+    
+    // Binary to char
+    private static char BinstrToChar(String binStr){ 
+        int[] temp = BinstrToIntArray(binStr); 
+        int sum = 0; 
+        for(int i = 0; i < temp.length; i++){ 
+            sum += temp[temp.length - 1 - i] << i; 
+        } 
+        return (char)sum; 
+    }
+
+    // String to Binary String, splited by space
+	private static String StrToBinstr(String str) {
+		char[] strChar = str.toCharArray();
+		String result = "";
+		for (int i = 0; i < strChar.length; i++) {
+			result += Integer.toBinaryString(strChar[i]) + " ";
+		}
+		return result;
+	}
 
     public static void main(String[] args) {
         System.out.println("OpenSSL avaliable: " + OpenSsl.isAvailable());
